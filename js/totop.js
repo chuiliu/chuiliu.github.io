de9c41bdfac829861a2b265b3081a308a2acc057
chuiliu <zhengchuiliu@foxmail.com>
2015-12-18 20:50:54 +0800

    Site updated: 2015-12-18 20:50:53

--- js/totop.js
+++ js/totop.js
@@ -1,18 +1,20 @@
 (function($) {
-	alert();
 	$(window).scroll(function(){
-		if ($(window).scrollTop()>100){
-			$("#to-top").fadeIn(500);
+		if ($(window).scrollTop() > 200){
+			// $("#to-top").fadeIn(500);
+			$("#to-top").animate({right:"30px"},"slow");
+			console.log(">   "+$(window).scrollTop());
 		}
 		else{
-			$("#to-top").fadeOut(500);
+			// $("#to-top").fadeOut(500);
+			$("#to-top").animate({right:"0px"},"slow");
+			console.log("<   "+$(window).scrollTop());
 		}
 	});
-	//当点击跳转链接后，回到页面顶部位置
 	$("#to-top").click(function(){
 		$('body,html').animate({
 			scrollTop:0
-		},1500);
+		},500);
 		return false;
 	});
 })(jQuery);
