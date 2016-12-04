a41758978f73cf04a11698e504fbd673422a5b48
chuiliu <zhengchuiliu@foxmail.com>
2016-12-04 17:37:48 +0800

    Site updated: 2016-12-04 17:37:46

--- js/index.js
+++ js/index.js
@@ -1,33 +1,68 @@
 (function($) {
+
     var lastTop = 0;
     var scrollTop = $(window).scrollTop();
     var minScroll = 30;
 
+    var $totop = $('#to-top');
+    var $mainHeader = $('#header .main-header-wrap');
+    var $subHeader = $('#header .sub-header-wrap');
+
     if (scrollTop < minScroll) {
-        $("#header .main-header-wrap").removeClass('hide');
+        $mainHeader.removeClass('hide');
     }
 
-    $(window).scroll(function(){
+    $totop.click(function() {
+        $('body, html').animate({
+            scrollTop:0
+        },300);
+        return false;
+    });
+    $('.icon-menu').on('click', function(e) {
+        e.stopPropagation();
+        $('.nav').show();
+        $(document).on('click', function() {
+            $('.nav').hide();
+        });
+    });
+    $('.nav').on('click', function(e) {
+        e.stopPropagation();
+    });
 
+    $(window).scroll(function(){
+        var mobile = window.matchMedia('(max-width: 768px)').matches;
         scrollTop = $(this).scrollTop();
 
-        if (lastTop < scrollTop && scrollTop > minScroll) {
-            // $("#header .main-header-wrap").hide();
-            // $("#header .sub-header-wrap").hide();
-            // $("#header .main-header-wrap").removeClass('hide');
-            $("#header .main-header-wrap").removeClass('main-header-show hide');
-            $("#header .sub-header-wrap").removeClass('sub-header-show');
-        } else if (lastTop > scrollTop && scrollTop > minScroll) {
-            // $("#header .sub-header-wrap").show();
-            $("#header .sub-header-wrap").addClass('sub-header-show');
-        } else {
-            // $("#header .main-header-wrap").show();
-            // $("#header .sub-header-wrap").hide();
-            $("#header .main-header-wrap").addClass('main-header-show');
-            $("#header .sub-header-wrap").removeClass('sub-header-show');
-
+        if (!mobile && lastTop < scrollTop && scrollTop > minScroll) {
+            $mainHeader.removeClass('main-header-show hide');
+            $subHeader.removeClass('sub-header-show');
+        } else if (!mobile && lastTop > scrollTop && scrollTop > minScroll) {
+            $subHeader.addClass('sub-header-show');
+        } else if (!mobile) {
+            $mainHeader.addClass('main-header-show');
+            $subHeader.removeClass('sub-header-show');
         }
 
         lastTop = scrollTop;
+
+        // 回顶部
+        if (scrollTop > 200){
+            $totop.stop().animate({right:'5px'});
+        } else{
+            $totop.stop().animate({right:'-50px'});
+        }
+
     });
+
+
+    $(window).scroll(function() {
+
+    });
+
+
+
+
+
+
+
 })(jQuery);
