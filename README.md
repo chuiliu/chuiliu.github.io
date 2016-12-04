24500474a38ae8f1074eb25a960858f412f5aafa
chuiliu <zhengchuiliu@foxmail.com>
2016-12-04 20:16:31 +0800

    Site updated: 2016-12-04 20:16:29

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # chuiliu.github.io
 基于GitHub Pages和Hexo搭建的静态blog
 
-个人博客，走走停停，写写记记
+个人博客，主题是自己折腾出来的，bug比较多，慢慢完善
 [chuiliu.github.io](http://chuiliu.github.io)
--- js/index.js
+++ js/index.js
@@ -20,19 +20,31 @@
     });
     $('.icon-menu').on('click', function(e) {
         e.stopPropagation();
-        $('.nav').show();
+        $('.main-header-wrap .nav').show();
         $(document).on('click', function() {
-            $('.nav').hide();
+            $('.main-header-wrap .nav').hide();
         });
     });
-    $('.nav').on('click', function(e) {
+    $('.main-header-wrap .nav').on('click', function(e) {
         e.stopPropagation();
     });
 
     $(window).scroll(function(){
-        $('.nav').hide();
-        var mobile = window.matchMedia('(max-width: 768px)').matches;
+
+        var mobile = false;
+        // 判断屏幕宽度
+        if (window.matchMedia) {
+            mobile = window.matchMedia('(max-width: 768px)').matches
+        } else {
+            mobile = $(window).width() <= 768 ? true : false;
+        }
+
+
         scrollTop = $(this).scrollTop();
+
+        if (mobile) {
+            $('.main-header-wrap .nav').hide();
+        }
 
         if (!mobile && lastTop < scrollTop && scrollTop > minScroll) {
             // 上滚
@@ -56,16 +68,6 @@
         }
 
     });
-
-
-    $(window).scroll(function() {
-
-    });
-
-
-
-
-
 
 
 })(jQuery);
