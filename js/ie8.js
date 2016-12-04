5330a7e08bc66b30975201b077f4e11135381bd9
chuiliu <zhengchuiliu@foxmail.com>
2016-12-04 19:42:51 +0800

    Site updated: 2016-12-04 19:42:49

---
+++ js/ie8.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    var header = document.getElementById('header');
+    var mainHeader = header.getElementsByTagName('div')[0];
+
+    mainHeader.style.zIndex = -1;
+    mainHeader.style.display = 'none';
+};
+
+
